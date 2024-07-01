d452857952618b49ea17bd18889ff05f0155eca9
sagakteok <maze77777@naver.com>
2024-07-01 16:30:19 +0900

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import { registerPlugins } from "./plugins/index.js";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
