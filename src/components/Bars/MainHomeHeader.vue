766a8c35a3ad62ec98825671b8a3c39b83fed4bc
sagakteok <maze77777@naver.com>
2024-07-12 15:58:30 +0900

    Add Header

---
+++ src/components/Bars/MainHomeHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app-bar :style="AppbarStyle()" :elevation="0">
+    <text :style="AppbarNavIconStyle()">
+      <v-app-bar-nav-icon/>
+    </text>
+    <text :style="TriPlandLogoStyle()">
+      <v-img :src="TriPlandLogo"/>
+    </text>
+    <text :style="AccountLogoStyle()">
+      <v-icon>mdi-account-outline</v-icon>
+    </text>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      TriPlandLogo: new URL('../../assets/TriPland.png', import.meta.url).href
+    }
+  },
+  methods: {
+    AppbarNavIconStyle(){
+      return{
+        marginLeft: '10px',
+        marginBottom: '-20px'
+      }
+    },
+    TriPlandLogoStyle(){
+      return{
+        width: '200px',
+        margin: 'auto',
+        marginBottom: '0px'
+      }
+    },
+    AccountLogoStyle(){
+      return{
+        width: '50px',
+        fontSize: '25px',
+        marginRight: '10px',
+        marginBottom: '-20px',
+        cursor: 'pointer'
+      }
+    },
+    AppbarStyle() {
+      return {
+        background: "#D094FF",
+        color: "#FFFFFF",
+        height: "350px"
+      };
+    }
+  }
+};
+</script>
--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -1,58 +1,14 @@
 <template>
+  <MainHomeHeader/>
   <Bottombar/>
-  <v-app-bar :style="AppbarStyle()" :elevation="0">
-    <text :style="AppbarNavIconStyle()">
-      <v-app-bar-nav-icon/>
-    </text>
-    <text :style="TriPlandLogoStyle()">
-      <v-img :src="TriPlandLogo"/>
-    </text>
-    <text :style="AccountLogoStyle()">
-      <v-icon>mdi-account-outline</v-icon>
-    </text>
-  </v-app-bar>
 </template>
 
 <script>
-import Bottombar from "./Bars/Bottombar.vue"; // Ensure this is at the top
+import Bottombar from "./Bars/Bottombar.vue";
+import MainHomeHeader from "./Bars/MainHomeHeader.vue"; // Ensure this is at the top
 
 export default {
-  components: {Bottombar},
-  data(){
-    return {
-      TriPlandLogo: new URL('../assets/TriPland.png', import.meta.url).href
-    }
-  },
-  methods: {
-    AppbarNavIconStyle(){
-      return{
-        marginLeft: '10px',
-        marginBottom: '-20px'
-      }
-    },
-    TriPlandLogoStyle(){
-      return{
-        width: '200px',
-        margin: 'auto',
-        marginBottom: '0px'
-      }
-    },
-    AccountLogoStyle(){
-      return{
-        width: '50px',
-        fontSize: '25px',
-        marginRight: '10px',
-        marginBottom: '-20px',
-        cursor: 'pointer'
-      }
-    },
-    AppbarStyle() {
-      return {
-        background: "#D094FF",
-        color: "#FFFFFF",
-        height: "350px"
-      };
-    }
-  }
-};
+  components: {MainHomeHeader, Bottombar}
+}
+
 </script>
