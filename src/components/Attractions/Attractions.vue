fb557368a18c7545e10322a516f4f982015c5fcf
SunjunSS <zinzza122@naver.com>
2024-07-18 10:26:37 +0900

    assets->homeimage파일 추가, Attractions파일생성->Attractions.vue추가, MainHome.vue변경

---
+++ src/components/Attractions/Attractions.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app>
+
+  <div class="text">오늘의 여행지</div>
+    <div class="center">
+      <v-carousel show-arrows="hover" hide-delimiters style="height: 70vh; width: 50vw;">
+        <v-carousel-item src="@/assets/homeimage/castle.jpg" cover>
+          <div class="attraction">
+            오사카성
+          </div>
+          <img class="carousel-image" src="@/assets/homeimage/castle.jpg" alt="Castle">
+        </v-carousel-item>
+
+        <v-carousel-item src="@/assets/homeimage/mountain.jpg" cover>
+          <div class="attraction">
+            설악산
+          </div>
+          <img class="carousel-image" src="@/assets/homeimage/mountain.jpg" alt="Mountain">
+        </v-carousel-item>
+
+        <v-carousel-item src="@/assets/homeimage/sea.jpg" cover>
+          <div class="attraction">
+            강릉 바다
+          </div>
+          <img class="carousel-image" src="@/assets/homeimage/sea.jpg" alt="Sea">
+        </v-carousel-item>
+      </v-carousel>
+    </div>
+
+  </v-app>
+</template>
+
+<style scoped>
+
+.text{
+  position: relative;
+  right: 50%;
+  top: 10%;
+  font-size: 40px;
+  font-weight: bold;
+  color: purple;
+}
+
+.center {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 8%;
+}
+
+.carousel-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.attraction {
+  position: relative;
+  top: 85%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+}
+</style>
--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -1,72 +1,13 @@
 <template>
   <v-app>
     <Icon/>
-    <div class="text">오늘의 여행지</div>
-    <div class="center">
-      <v-carousel show-arrows="hover" hide-delimiters style="height: 60vh; width: 50vw;">
-        <v-carousel-item src="/homeimage/castle.jpg" cover>
-          <div class="attraction">
-            오사카성
-          </div>
-          <img class="carousel-image" src="/homeimage/castle.jpg" alt="Castle">
-        </v-carousel-item>
-
-        <v-carousel-item src="/homeimage/mountain.jpg" cover>
-          <div class="attraction">
-            설악산
-          </div>
-          <img class="carousel-image" src="/homeimage/mountain.jpg" alt="Mountain">
-        </v-carousel-item>
-
-        <v-carousel-item src="/homeimage/sea.jpg" cover>
-          <div class="attraction">
-            강릉 바다
-          </div>
-          <img class="carousel-image" src="/homeimage/sea.jpg" alt="Sea">
-        </v-carousel-item>
-      </v-carousel>
-    </div>
+    <Attractions/>
     <Bottombar/>
   </v-app>
 </template>
 
-<style scoped>
-
-.text{
-  position: static;
-  left: 18%;
-  top: 13%;
-  font-size: 40px;
-  font-weight: bold;
-  color: purple;
-}
-
-.center {
-  position: static;
-  left: 25%;
-  top: 25%;
-}
-
-.carousel-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.attraction {
-  position: absolute;
-  top: 85%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  color: white;
-  padding: 20px;
-  font-size: 25px;
-  font-weight: bold;
-}
-</style>
-
 <script setup>
 import Bottombar from "./Bars/Bottombar.vue";
 import Icon from "./icons/Icon.vue";
+import Attractions from "./Attractions/Attractions.vue";
 </script>
