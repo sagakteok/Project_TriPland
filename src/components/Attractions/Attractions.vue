50091244ecb3397e64075909737e61daf4d047f9
sagakteok <maze77777@naver.com>
2024-08-07 23:21:49 +0900

    Fix Attraction & Attraction Title, Add transitions

--- src/components/Attractions/Attractions.vue
+++ src/components/Attractions/Attractions.vue
@@ -1,59 +1,40 @@
 <template>
-  <div style="max-width: 1600px; margin: auto">
+  <div style="max-width: 1600px; width: 60vw; margin: auto; margin-top: 70px">
     <v-row>
-      <div class="text">오늘의 여행지</div>
-    </v-row>
-    <v-row>
-      <v-carousel show-arrows="hover" hide-delimiters cycle interval="3000" style="height: 60vh; width: 40vw; margin: auto">
-        <v-carousel-item src="@/assets/homeimage/castle.jpg" cover>
-          <div class="attraction">
-            오사카성
-          </div>
-          <img class="carousel-image" src="@/assets/homeimage/castle.jpg" alt="Castle">
-        </v-carousel-item>
-
-        <v-carousel-item src="@/assets/homeimage/mountain.jpg" cover>
-          <div class="attraction">
-            설악산
-          </div>
-          <img class="carousel-image" src="@/assets/homeimage/mountain.jpg" alt="Mountain">
-        </v-carousel-item>
-
-        <v-carousel-item src="@/assets/homeimage/sea.jpg" cover>
-          <div class="attraction">
-            강릉 바다
-          </div>
-          <img class="carousel-image" src="@/assets/homeimage/sea.jpg" alt="Sea">
-        </v-carousel-item>
-      </v-carousel>
+      <v-scroll-y-reverse-transition>
+        <div class="text" v-show="Transition1">오늘의 여행지</div>
+      </v-scroll-y-reverse-transition>
+      <v-dialog-transition>
+        <v-carousel v-show="Transition2" show-arrows="hover" hide-delimiters cycle interval="3000" style="height: 40vh; width: 100vw; border-radius: 40px; margin-top: 20px; box-shadow: 0px 0px 10px #00000050;">
+          <v-carousel-item :src="castle" cover>
+            <div class="attraction">
+              오사카성
+            </div>
+          </v-carousel-item>
+          <v-carousel-item :src="mountain" cover>
+            <div class="attraction">
+              설악산
+            </div>
+          </v-carousel-item>
+          <v-carousel-item :src="sea" cover>
+            <div class="attraction">
+              강릉 바다
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-dialog-transition>
     </v-row>
   </div>
 </template>
 
 <style>
-
 .text{
-  position: relative;
-  left: 5%;
-  top: 50px;
+  text-align: left;
+  font-family: Inter-Bold, Helvetica;
   font-size: 35px;
-  font-weight: bold;
-  color: purple;
+  font-weight: 400;
+  color: #A532FF;
 }
-
-.center {
-  position: relative;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: 2%;
-}
-
-.carousel-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
 .attraction {
   position: relative;
   top: 85%;
@@ -62,9 +43,30 @@
   text-align: center;
   color: white;
   padding: 20px;
-  font-size: 25px;
-  font-weight: bold;
+  font-size: 40px;
+  font-weight: 500;
 }
 </style>
-<script setup>
+
+<script>
+export default {
+  name: 'Attractions',
+  data() {
+    return {
+      Transition1: false,
+      Transition2: false,
+      castle: new URL('../../assets/homeimage/castle.jpg', import.meta.url).href,
+      mountain: new URL('../../assets/homeimage/mountain.jpg', import.meta.url).href,
+      sea: new URL('../../assets/homeimage/sea.jpg', import.meta.url).href,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.Transition1 = true;
+    }, 100)
+    setTimeout(() => {
+      this.Transition2 = true;
+    }, 200)
+  }
+}
 </script>
--- src/components/icons/Icon.vue
+++ src/components/icons/Icon.vue
@@ -1,49 +1,61 @@
 <template>
-  <div style="max-width: 1600px; margin: auto; margin-top: 50px">
-    <div class="text-center">
+  <v-scroll-y-transition>
+    <div style="max-width: 1600px; margin: auto; margin-top: 80px">
       <div class="icon-container">
-        <div class="icon-item">
-          <v-hover v-slot="{ isHovering, props }">
-            <v-btn
-                class="ma-2"
-                :style="IconButtonStyle(isHovering)"
-                icon="mdi-map-marker"
-                v-bind="props"
-            ></v-btn>
-          </v-hover>
-          <div class="icon-text"><b> 여행지 추천 </b></div>
-        </div>
-
-        <div class="icon-item">
-          <v-hover v-slot="{ isHovering, props }">
-            <v-btn
-                class="ma-2"
-                :style="IconButtonStyle(isHovering)"
-                icon="mdi-car"
-                v-bind="props"
-            ></v-btn>
-          </v-hover>
-          <div class="icon-text"><b> 렌트카 </b></div>
-        </div>
-        <div class="icon-item">
-          <v-hover v-slot="{ isHovering, props }">
-            <v-btn
-                class="ma-2"
-                :style="IconButtonStyle(isHovering)"
-                icon="mdi-home-modern"
-                v-bind="props"
-            ></v-btn>
-          </v-hover>
-          <div class="icon-text"><b> 숙소 예약 </b></div>
-        </div>
+        <v-scroll-y-transition>
+          <div v-show="Transition1" class="icon-item">
+            <v-hover v-slot="{ isHovering, props }">
+              <v-btn
+                  class="ma-2"
+                  :style="IconButtonStyle(isHovering)"
+                  icon="mdi-map-marker"
+                  v-bind="props"
+              ></v-btn>
+            </v-hover>
+            <div class="icon-text"><b> 여행지 추천 </b></div>
+          </div>
+        </v-scroll-y-transition>
+        <v-scroll-y-transition>
+          <div v-show="Transition2" class="icon-item">
+            <v-hover v-slot="{ isHovering, props }">
+              <v-btn
+                  class="ma-2"
+                  :style="IconButtonStyle(isHovering)"
+                  icon="mdi-car"
+                  v-bind="props"
+              ></v-btn>
+            </v-hover>
+            <div class="icon-text"><b> 렌트카 </b></div>
+          </div>
+        </v-scroll-y-transition>
+        <v-scroll-y-transition>
+          <div v-show="Transition3" class="icon-item">
+            <v-hover v-slot="{ isHovering, props }">
+              <v-btn
+                  class="ma-2"
+                  :style="IconButtonStyle(isHovering)"
+                  icon="mdi-home-modern"
+                  v-bind="props"
+              ></v-btn>
+            </v-hover>
+            <div class="icon-text"><b> 숙소 예약 </b></div>
+          </div>
+        </v-scroll-y-transition>
       </div>
     </div>
-  </div>
+  </v-scroll-y-transition>
 </template>
 
 <script>
 export default {
   name: 'Icon',
+  data() {
+    return {
+      Transition1: false,
+      Transition2: false,
+      Transition3: false
+    }
+  },
   methods: {
     IconButtonStyle(isHovering){
       return{
@@ -51,6 +63,17 @@ export default {
         color: isHovering ? '#A533FF' : '#000000',
       }
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.Transition1 = true;
+    }, 400)
+    setTimeout(() => {
+      this.Transition2 = true;
+    }, 430)
+    setTimeout(() => {
+      this.Transition3 = true;
+    }, 460)
   }
 }
 </script>
