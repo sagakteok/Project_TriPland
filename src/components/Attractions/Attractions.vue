ad5480155d9cc0821f0c8b2ebdfc446d90fd6d06
SunjunSS <zinzza122@naver.com>
2024-08-08 13:10:55 +0900

    Attractions.vue 수정, ignore 수정

--- src/components/Attractions/Attractions.vue
+++ src/components/Attractions/Attractions.vue
@@ -5,18 +5,21 @@
         <div class="text" v-show="Transition1">오늘의 여행지</div>
       </v-scroll-y-reverse-transition>
       <v-dialog-transition>
-        <v-carousel v-show="Transition2" show-arrows="hover" hide-delimiters cycle interval="3000" style="height: 40vh; width: 100vw; border-radius: 40px; margin-top: 20px; box-shadow: 0px 0px 10px #00000050;">
-          <v-carousel-item :src="castle" cover>
+        <v-carousel v-show="Transition2" show-arrows="hover" hide-delimiters cycle interval="3000" style="height: 45vh; width: 100vw; border-radius: 40px; margin-top: 20px; box-shadow: 0px 0px 10px #00000050;">
+          <v-carousel-item>
+            <img :src="castle" class="carousel-image" alt="Castle">
             <div class="attraction">
               오사카성
             </div>
           </v-carousel-item>
-          <v-carousel-item :src="mountain" cover>
+          <v-carousel-item>
+            <img :src="mountain" class="carousel-image" alt="Mountain">
             <div class="attraction">
               설악산
             </div>
           </v-carousel-item>
-          <v-carousel-item :src="sea" cover>
+          <v-carousel-item>
+            <img :src="sea" class="carousel-image" alt="Sea">
             <div class="attraction">
               강릉 바다
             </div>
@@ -28,7 +31,7 @@
 </template>
 
 <style>
-.text{
+.text {
   text-align: left;
   font-family: Inter-Bold, Helvetica;
   font-size: 35px;
@@ -36,15 +39,21 @@
   color: #A532FF;
 }
 .attraction {
-  position: relative;
-  top: 85%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  color: white;
-  padding: 20px;
-  font-size: 40px;
-  font-weight: 500;
+  position: absolute !important;
+  bottom: 20px !important;
+  left: 50% !important;
+  transform: translateX(-50%) !important;
+  text-align: center !important;
+  color: white !important;
+  padding: 20px !important;
+  font-size: 35px !important;
+  font-weight: 500 !important;
+}
+.carousel-image {
+  width: 100%;
+  height: 100%;
+  object-fit: fill; 
+  border-radius: 40px;
 }
 </style>
 
