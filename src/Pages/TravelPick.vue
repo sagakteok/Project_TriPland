e67deb97e65ac694f48b7dd87a8930fded181c7e
JeonHyunGyu <hyungu2001@naver.com>
2024-08-12 21:32:21 +0900

    change TravelPick.vue

--- src/Pages/TravelPick.vue
+++ src/Pages/TravelPick.vue
@@ -2,15 +2,53 @@
   <div v-if="Width500">
     <PagesHeader500/>
     <Bottombar500/>
+    <div class="travel-container500">
+        <div class="TravelText500">여행지 추천</div>
+        <div class="content-box">
+            <div class="info500">
+                <v-icon color="purple" class="icon">mdi-lightbulb-on-outline</v-icon>
+                <span>여행을 원하시는 지역을 선택해주세요</span>
+            </div>
+            <div class="button-group">
+                <v-btn class="country-btn" color="purple" outlined>국내</v-btn>
+                <v-btn class="country-btn" color="purple" outlined>해외</v-btn>
+            </div>
+        </div>
+    </div>
   </div>
   <div v-else-if="Width800">
     <PagesHeader800/>
     <Bottombar800/>
+    <div class="travel-container">
+    <div class="TravelText800">여행지 추천</div>
+    <div class="content-box">
+            <div class="info800">
+              <v-icon color="purple" class="icon">mdi-lightbulb-on-outline</v-icon>
+              <span>여행을 원하시는 지역을 선택해주세요</span>
+            </div>
+            <div class="button-group">
+              <v-btn class="country-btn" color="purple" outlined>국내</v-btn>
+              <v-btn class="country-btn" color="purple" outlined>해외</v-btn>
+            </div>
+          </div>
+    </div>
   </div>
   <div v-else>
     <PagesHeader1600/>
     <Bottombar1600/>
-    <div class="TravelText">여행지 추천</div>
+    <div class="travel-container">
+        <div class="TravelText1600">여행지 추천</div>
+        <div class="content-box">
+            <div class="info1600">
+              <v-icon color="purple" class="icon">mdi-lightbulb-on-outline</v-icon>
+              <span>여행을 원하시는 지역을 선택해주세요</span>
+            </div>
+            <div class="button-group">
+              <v-btn class="country-btn" color="purple" outlined>국내</v-btn>
+              <v-btn class="country-btn" color="purple" outlined>해외</v-btn>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -52,7 +90,27 @@ export default {
 </script>
 
 <style>
-.TravelText{
+.TravelText500{
+  text-align: left;
+  font-family: Inter-Bold;
+  font-size: 180%;
+  font-weight: 1000;
+  color: #7000FF;
+  position: fixed;
+  left: 6%;
+  top: 12%;
+}
+.TravelText800{
+  text-align: left;
+  font-family: Inter-Bold;
+  font-size: 220%;
+  font-weight: 1000;
+  color: #7000FF;
+  position: fixed;
+  left: 6%;
+  top: 12%;
+}
+.TravelText1600{
   text-align: left;
   font-family: Inter-Bold;
   font-size: 250%;
@@ -61,5 +119,56 @@ export default {
   position: fixed;
   left: 6%;
   top: 12%;
+}
+.travel-container {
+  max-width: 300px;
+  margin: 0 auto;
+  text-align: center;
+}
+.content-box {
+  background-color: #f4f4f4;
+  padding: 5%;
+  border-radius: 12px;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 50%;
+}
+.info500 {
+  display: flex;
+  align-items: center;
+  margin-top: -2%;
+  margin-bottom: 30%;
+  font-size: 110%;
+  color: #333;
+}
+.info800 {
+  display: flex;
+  align-items: center;
+  margin-top: -2%;
+  margin-bottom: 16%;
+  font-size: 120%;
+  color: #333;
+}
+.info1600 {
+  display: flex;
+  align-items: center;
+  margin-top: -2%;
+  margin-bottom: 14%;
+  font-size: 150%;
+  color: #333;
+}
+.icon {
+  margin-right: 10px;
+}
+.button-group {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 16px;
+}
+.country-btn {
+  width: 20%;
 }
 </style>
