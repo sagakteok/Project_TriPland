7795a3f2760aa6fdfa698f4ec30f82f775d72a9c
SunjunSS <zinzza122@naver.com>
2024-08-24 23:19:46 +0900

    App.vue padding 변경

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-main style="background-color: #F8F8F8">
+    <v-main style="background-color: #F8F8F8; padding-bottom: 0px;">
       <router-view/>
     </v-main>
   </v-app>
