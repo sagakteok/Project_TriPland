79b5d8ebd447951028238f9c653c542830cd88c4
sagakteok <maze77777@naver.com>
2024-08-24 23:17:21 +0900

    Add Capacitor.js

---
+++ android/app/src/main/java/com/TriPland/MainActivity.java
@@ -0,0 +1,5 @@
+package com.TriPland;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <v-app>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <v-app style="background-color: transparent">
     <v-main style="background-color: #F8F8F8">
       <router-view/>
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  mounted() {
+    document.documentElement.addEventListener('touchstart', function (event) {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    }, false);
+
+    var lastTouchEnd = 0;
+
+    document.documentElement.addEventListener('touchend', function (event) {
+      var now = (new Date()).getTime();
+      if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+      } lastTouchEnd = now;
+    }, false);
+  }
+}
+</script>
